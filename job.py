f6612e1889ee8e67e5b8fc4df5d4de5df5caf843
omegascorp <omegascorp@gmail.com>
2015-08-12 23:37:20 +0400

    Post folders

--- job.py
+++ job.py
@@ -3,39 +3,51 @@ import urllib.parse
 import json
 import requests
 import os
+import os.path
 import logging
 import random
 import time
 import sys
+import shutil
 
 def request(url, data):
     res = requests.get(url + '?' + urllib.parse.urlencode(data))
     return json.loads(res.content.decode())
-def upload(url, path, photo):
+def uploadPhoto(url, path, photo):
     f = open(path+photo, 'rb')
     fileName = '1.'+parseExtension(photo)
     res = requests.post(url, files={'photo': ('1.'+fileName, f)})
     f.close()
     return json.loads(res.content.decode())
-def parseTags(fileName):
-    start = fileName.find('#');
-    end = fileName.rfind('.');
+def uploadAndSavePhoto(url, path, photo):
+    response = uploadPhoto(url, path, photo)
+    response = savePhoto(response['server'], response['photo'], response['hash'])
+    return response['response'][0]['id']
+def parseTags(fileName, isDir):
+    start = fileName.find('#')
+    if isDir:
+        end = len(fileName)
+    else:
+        end = fileName.rfind('.')
     if(start != -1 and end != -1):
         return fileName[start:end]
     return ''
-def parseText(fileName):
-    start = fileName.find('!');
-    sharp = fileName.find('#');
-    end = fileName.rfind('.');
+def parseText(fileName, isDir):
+    start = fileName.find('!')
+    sharp = fileName.find('#')
+    if isDir:
+        end = len(fileName)
+    else:
+        end = fileName.rfind('.')
     text = ''
     if(start != -1 and sharp != -1):
         text = fileName[start+1:sharp]
     elif(start != -1 and end != -1):
         text = fileName[start+1:end]
-    text = text.replace('|', '/');
+    text = text.replace('|', '/')
     return text
 def parseExtension(fileName):
-    start = fileName.rfind('.');
+    start = fileName.rfind('.')
     if(start != -1):
         return fileName[start+1:]
     return ''
@@ -58,10 +70,18 @@ def savePhoto(server, photo, responseHash):
     })
     logging.info(response)
     return response
-def savePost(photoId, text, lastPublishDate):
+def savePost(photoIds, text, lastPublishDate):
+    attachments = '';
+    if isinstance(photoIds, list):
+        for photoId in photoIds:
+            if len(attachments):
+                attachments += ',';
+            attachments += 'photo' + config['userId'] + '_' + str(photoId)
+    else:
+        attachments += 'photo' + config['userId'] + '_' + str(photoIds)
     response = request('https://api.vk.com/method/wall.post', {
         'owner_id': '-' + config['groupId'],
-        'attachments': 'photo' + config['userId'] + '_' + str(photoId),
+        'attachments': attachments,
         'from_group': 1,
         'access_token': config['accessToken'],
         'message': text,
@@ -70,36 +90,39 @@ def savePost(photoId, text, lastPublishDate):
     })
     logging.info(response)
 
-def uploadRandomImage(path):
+def uploadRandomPost(path):
     imagesPath = path + 'images/'
     images = os.listdir(imagesPath)
     print(random.randint(0,len(images)-1))
     image = images[random.randint(0,len(images)-1)]
-
     if data['lastPublishDate'] < int(time.time()):
         data['lastPublishDate'] = int(time.time())
     else:
-        data['lastPublishDate'] += 60 * 60;
+        data['lastPublishDate'] += 60 * config['period'];
 
     response = getUploadServer()
     uploadUrl = response['response']['upload_url']
+    isDir = os.path.isdir(imagesPath + image)
 
-    response = upload(uploadUrl, imagesPath, image)
-    server = response['server']
-    responseHash = response['hash']
-    photo = response['photo']
+    if isDir:
+        photoIds = []
+        for img in os.listdir(imagesPath + image):
+            photoId = uploadAndSavePhoto(uploadUrl, imagesPath + image + '/' , img)
+            photoIds.append(photoId)
+    else:
+        photoIds = uploadAndSavePhoto(uploadUrl, imagesPath, image)
 
-    response = savePhoto(server, photo, responseHash)
-    photoId = response['response'][0]['id']
-
-    text = parseText(image);
+    text = parseText(image, isDir)
     if text:
         text += '\n'
     if len(config['tags']):
-        text += config['tags'] + ' ';
-    text += parseTags(image)
-    response = savePost(photoId, text, data['lastPublishDate'])
-    os.remove(imagesPath + image)
+        text += config['tags'] + ' '
+    text += parseTags(image, isDir)
+    response = savePost(photoIds, text, data['lastPublishDate'])
+    if isDir:
+        shutil.rmtree(imagesPath + image)
+    else:
+        os.remove(imagesPath + image)
 
 path = os.path.dirname(os.path.abspath(__file__)) + '/'
 logging.basicConfig(filename=path + 'log',level=logging.INFO)
@@ -113,7 +136,7 @@ if len(sys.argv) >= 2:
     runsCount = int(sys.argv[1])
 
 for i in range(runsCount):
-    uploadRandomImage(path)
+    uploadRandomPost(path)
 
 with open(path + 'data.json', 'w') as outFile:
     json.dump(data, outFile)
